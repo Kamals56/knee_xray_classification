71e847161ed4ebd5bd709199a4ae1d70e5bf7990
kamallamichhanes56 <kamallamichhane222@gmail.com>
2025-12-05 12:45:10 +0200

    updated args

--- src/args.py
+++ src/args.py
@@ -1,12 +1,15 @@
 import argparse
+import os
 
 def get_args():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
     parser = argparse.ArgumentParser(description="Model training options")
 
-    parser.add_argument("-backbone", type = str, default = "resnet50", 
+    parser.add_argument("-backbone", type = str, default = "resnet18", 
                         choices=["resnet18", "resnet34", "resnet50"])
     
-    parser.add_argument("-csv_dir", type = str, default = "/home/kamal/theme3/structured_project/csv_datasets")
+    parser.add_argument("-csv_dir", type = str, default=os.path.join(BASE_DIR, "../data/csv_datasets"))
     
     parser.add_argument("-batch_size", type= int, default = "32", 
                         choices = [16, 32, 64])
@@ -15,10 +18,10 @@ def get_args():
     
     parser.add_argument("-epochs" , default = 10)
 
-    parser.add_argument("-out_dir", type = str, default = "session")
+    parser.add_argument("-out_dir", type = str, default=os.path.join(BASE_DIR, "../session"))
 
-    parser.add_argument("-num_workers", type = int, default = 8,
-                        choices = [4, 6, 8])
+    parser.add_argument("-n_splits", type=int, default=5, help="Number of folds for cross-validation")
+
     args = parser.parse_args()
     
     return args
